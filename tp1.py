9f6bdf1abf2042b2deb4ce69c2f96f9d424ac208
LuzGodoy <godoydq@gmail.com>
2023-04-02 19:27:07 -0300

    + first commit

---
+++ tp1.py
@@ -0,0 +1,12 @@
+
+# Trabajo Práctico número 1
+# Alumnos: LuzGodoy y GuillermoArregin
+
+# Un programa que permita generar valores simulados que sigan una
+# determinada distribución de probabilidad.
+
+# tp1 prob_dist.csv values.csv n [-d] [-x]
+# n -> Cantidad de valores que se desean generar
+# d -> Flag para mostrar el estadistico de prueba ks
+# x -> Flag para mostrar el estadistico de prueba ji cuadrado
+
