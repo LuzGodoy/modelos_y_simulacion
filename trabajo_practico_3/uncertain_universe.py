67e81e5812dc8be70b4e7c086412c398fb686b38
LuzGodoy <godoydq@gmail.com>
2023-05-10 23:25:10 -0300

    + implement wald method

--- trabajo_practico_3/uncertain_universe.py
+++ trabajo_practico_3/uncertain_universe.py
@@ -32,14 +32,29 @@ def analize_by_laplace(matrix, n, m):
   max_index = results.argmax()
   min_index = results.argmin()
   print(colored(f"\nProblema de Beneficios:\nSe recomienda elegir la alternativa {max_index}", 'cyan'))
-  print(colored(f"Que presenta una promedio de {results[max_index]}",'cyan'))
+  print(colored(f"Que presenta un promedio de {results[max_index]}",'cyan'))
   print(colored(f"\nProblema de Costos:\nSe recomienda elegir la alternativa {min_index}", 'light_blue'))
-  print(colored(f"Que presenta una promedio de {results[min_index]}",'light_blue'))
+  print(colored(f"Que presenta un promedio de {results[min_index]}",'light_blue'))
   
   return
 
 def analize_by_wald(matrix):
-
+  '''
+  Wald asume that the worst is possible to happend. 
+  For cases on we're analizing benefits, it's used maxxminy c(x,y)
+  For cases of costs, it's used minxmaxy c(x,y)'''
+  print(colored("\n\nMetodo de Wald", 'magenta'))
+  minx=[]; maxx=[]
+  for index, row in matrix.iterrows():
+    array= np.array(row)
+    minx.append(np.min(array)) # for benefits
+    maxx.append(np.max(array)) # for costs
+  maxy=np.argmax(minx)
+  miny=np.argmin(maxx)
+  print(colored(f"\nProblema de Beneficios:\nSe recomienda elegir la alternativa {maxy}", 'cyan'))
+  print(colored(f"Que presenta un coeficiente de {minx[maxy]}",'cyan'))
+  print(colored(f"\nProblema de Costos:\nSe recomienda elegir la alternativa {miny}", 'light_blue'))
+  print(colored(f"Que presenta un coeficiente de {maxx[miny]}",'light_blue'))
   return
 
 def analize_by_hurwicz(matrix):
